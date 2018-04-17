a88e9ef259b29dcbe01d580a7698bad0a3a28866
jimmycgz <38402619+jimmycgz@users.noreply.github.com>
2018-04-16 21:53:35 -0400

    Update index.php

--- src/index.php
+++ src/index.php
@@ -10,6 +10,7 @@
       <a href="index.php">Greet someone else</a>
     <? } else { ?>
       <form class="greetingForm" action="index.php" method="post">
+        <label for="who">This is run from Docker 1</label>
         <label for="who">Say hello to</label>
         <input type="text" name="who">
         <input type="submit" name="greet" value="Say Hello">
