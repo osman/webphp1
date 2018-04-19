217114135ef05e72d8b45e22985115ec024b7311
jimmycgz <38402619+jimmycgz@users.noreply.github.com>
2018-04-18 23:14:30 -0400

    Update Move_Repo_to_docker.bash

--- Move_Repo_to_docker.bash
+++ Move_Repo_to_docker.bash
@@ -15,23 +15,3 @@ docker rm -f docker-php-hello  || true
 
 docker build -t webphp1hello /home/jimmycgz/webphp1/
 docker run --name=docker-php-hello -d -it -p 80:80 webphp1hello
-
-
-#Deploy to GCP Ubuntu via ssh
-ssh -i "/home/ubuntu/Jmy_Key_GCP_Apr_2018.pem" jimmycgz@35.185.13.195
-
-cd /home/jimmycgz 
-rm -rf webphp1
-mkdir -p /home/jimmycgz/webphp1
-cd /home/jimmycgz/webphp1
-git init
-git remote add origin https://github.com/jimmycgz/webphp1.git
-git pull origin master
-
-
-docker rm -f docker-php-hello  || true 
-
-docker build -t webphp1hello /home/jimmycgz/webphp1/
-docker run --name=docker-php-hello -d -it -p 80:80 webphp1hello
-
-#docker image rm -f $(docker images -aq)
